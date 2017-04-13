967321085c3b47458c08afcc63bc1ce0168a0392
alliekim13 <alliekim@hawaii.edu>
2017-04-12 21:14:32 -1000

    Minor reformatting of test case output

--- src/com/vor/driver/Driver.java
+++ src/com/vor/driver/Driver.java
@@ -15,11 +15,12 @@ public class Driver {
 
 		testCalculateDeflection(178, 180, false);
 		testCalculateDeflection(182, 180, false);
-		testCalculateDeflection(7, 178, true);
+    testCalculateDeflection(7, 178, true);
 		testCalculateDeflection(10, 178, true);
 		testCalculateDeflection(189, 178, true);
 
-		testisTo(180, 360);
+
+    testisTo(180, 360);
 		testisTo(180, 180);
 		testisTo(0, 360);
 		testisTo(360, 0); 
@@ -31,7 +32,7 @@ public class Driver {
 	}
 
 	public static void testVORCalculator(RadioSignal radioSignal, int obs) {
-    System.out.println("Testing calculate With parameters");
+    System.out.println("\n\nTesting calculate With parameters");
     System.out.println("RadioSignal: "+ radioSignal);
     System.out.println("OBS: "+ obs);
     VORCalculator calc = new VORCalculator(new VOR());
@@ -40,7 +41,7 @@ public class Driver {
   }
 
 	public static void testIsAbeam(int radial, int obs) {
-    System.out.println("Testing isAbeam With parameters");
+    System.out.println("\n\nTesting isAbeam With parameters");
     System.out.println("Radial: "+ radial);
     System.out.println("OBS: "+ obs);
     RadioSignal rs = new RadioSignal(radial, Signal.GOOD);
@@ -50,7 +51,7 @@ public class Driver {
 		System.out.println(abeam);
 	}
 	public static void testCalculateDeflection(int radial, int obs, boolean isTo) {
-    System.out.println("Testing calculateDeflection With parameters");
+    System.out.println("\n\nTesting calculateDeflection With parameters");
     System.out.println("Radial: "+ radial);
     System.out.println("OBS: "+ obs);
     System.out.println("isTo: "+ isTo);
@@ -61,6 +62,9 @@ public class Driver {
   }
 
   public static void testisTo(int heading, int radial) {
+    System.out.println("\n\nTesting isTo With parameters");
+    System.out.println("Heading: "+heading);
+    System.out.println("Radial: "+radial);
     VORCalculator vc = new VORCalculator(new VOR());
     boolean result = vc.isTo(heading, radial);
     System.out.println(result);
